1dcc5f22b0ce14c195cc3495fc8c8ea347f3a84f
ghthor <ghthor@ghthor-laptop.localdomain>
2010-03-22 14:43:33 -0400

    Having a design BrainFuck, Don't know where I'm Going ATM -Issues, Whats really the point of a Thread safe List that has 	concurrent insertion and Reading -Indexed SortedList of strings would be best implemented with Maps

--- SortedList.go
+++ SortedList.go
@@ -13,57 +13,80 @@ type Worker interface {
 type WorkerPool struct {
 	size int
 	//workers []Worker
-	workers container.Vector
+	workers vector.Vector
 }
 
 func (wp *WorkerPool) init() {
 	//wp.workers = make([]Worker, wp.size)
-	size = 100
-	wp.workers = new(container.Vector)
+	wp.size = 100
 }
 
-func (wp *WorkerPool) PushWorker(worker *Worker) {
-	wp.workers
+func (wp *WorkerPool) PushWorker(worker Worker) {
+	wp.workers.Push(worker)
+	worker.Start()
 }
 
 func (wp *WorkerPool) Start() {
 }
 
 func (wp *WorkerPool) Stop() {
+	for _, worker := range wp.workers {
+		worker.(Worker).Stop()
+	}
 }
 
-type IntNode struct {
-	val int
-	parent *IntList
-	next *IntNode
-	prev *IntNode
-	begin *IntNode
-	end *IntNode
+type Node struct {
+	val interface {}
+	setVal chan int
+	getVal chan int
+	parent *SortedList
+	next *Node
+	prev *Node
+	begin *Node
+	end *Node
 	index uint32
 	lock sync.Mutex
 
-	stop chan int
-	setNext chan *IntNode
-	setPrev chan *IntNode
+	status string
+	getStatus chan string
+
+	stop chan bool
+	setNext chan *Node
+	setPrev chan *Node
 }
 
-func (i *IntNode) init(parent *IntList) {
+func NewNode(val interface {}, parent *SortedList) (newNode *Node) {
+	newNode = new(Node)
+	newNode.val = val
+	newNode.init(parent)
+}
+
+func (i *Node) init(parent *SortedList) {
 	i.parent = parent
-	i.stop = make(chan int)
-	i.setNext = make(chan *IntNode)
-	i.setPrev = make(chan *IntNode)
+	i.stop = make(chan bool)
+	i.getVal = make(chan int)
+	i.setVal = make(chan int)
+	i.getStatus = make(chan int)
+	i.setNext = make(chan *Node)
+	i.setPrev = make(chan *Node)
 }
 
-func (i *IntNode) Start() {
+func (i *Node) Start() {
 	go func() {
+		i.status = "started"
 		for {
+			i.status = "paused"
 			select {
+				case i.getStatus <- i.status:
 				case <- i.stop:
+					i.status = "stopped"
 					return
 				case next := <-i.setNext:
+					i.status = "working"
 					//next.prev = i
 					i.next = next
 				case prev :=  <-i.setPrev:
+					i.status = "working"
 					//prev.next = i
 					i.prev = prev
 			}
@@ -71,6 +94,55 @@ func (i *IntNode) Start() {
 	}()
 }
 
-type IntList interface {
-	At(index uint32) (*IntNode)
+func (i *Node) SetVal(val int) {
+	go func() {
+		i.setVal <- val
+	}()
 }
+
+func (i *Node) GetVal() (ret chan int) {
+	ret = make(chan int)
+	go func() {
+		temp := <-getVal
+		ret <- temp
+	}()
+	return ret
+}
+
+func (i *Node) SetNext(next *Node) {
+	go func() {
+		i.setNext <- next
+	}()
+}
+
+func (i *Node) SetPrev(prev *Node) {
+	go func() {
+		i.setPrev <- prev
+	}()
+}
+
+func (i *Node) Stop() {
+	go func() {
+		i.stop <- 0
+	}()
+}
+
+type SortedList struct {
+	WorkerPool
+	begin *Node
+	end *Node
+	len int
+}
+
+func (il *SortedList) Insert(val int) {
+	if il.begin == nil {
+		il.begin = NewNode(val, il)
+		il.PushWorker(il.begin)
+	} else if il.end == nil {
+		il.end = NewNode(val, il)
+		il.PushWorker(il.end)
+		il.begin.SetNext(il.end)
+		il.end.SetPrev(il.begin)
+	}
+}
+
