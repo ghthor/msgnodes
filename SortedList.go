26d82572f4ad44a71feaa2499b2c60b335ca7af5
ghthor <ghthor@ghthor-netbook.localdomain>
2010-03-25 13:39:57 -0400

    Fleshing out everything -Added Comparable interface -Added Comparable int type -Added go func to node that listens for setting and sending out val stored in that node

--- SortedList.go
+++ SortedList.go
@@ -35,10 +35,20 @@ func (wp *WorkerPool) Stop() {
 	}
 }
 
+type Comparable interface {
+	LessThan(comp Comparable) bool
+}
+
+type CompInt int
+
+func (ci *int) LessThan(comp Comparable) bool {
+	return (ci < comp)
+}
+
 type Node struct {
-	val interface {}
-	setVal chan int
-	getVal chan int
+	val Comparable
+	setVal chan Comparable
+	getVal chan Comparable
 	parent *SortedList
 	next *Node
 	prev *Node
@@ -50,7 +60,8 @@ type Node struct {
 	status string
 	getStatus chan string
 
-	stop chan bool
+	stop chan int
+	valServerStop chan int
 	getNext chan *Node
 	getPrev chan *Node
 	setNext chan *Node
@@ -63,11 +74,12 @@ func NewNode(val interface {}, parent *SortedList) (newNode *Node) {
 	newNode.init(parent)
 }
 
-func (i *Node) init(parent *SortedList) {
+func (i *Node) init(parent (*SortedList) {
 	i.parent = parent
 	i.stop = make(chan int)
-	i.getVal = make(chan interface {})
-	i.setVal = make(chan interface {})
+	i.valServerStop = make(chan int)
+	i.getVal = make(chan Comparable)
+	i.setVal = make(chan Comparable)
 	i.getStatus = make(chan string)
 	i.getNext = make(chan *Node)
 	i.getPrev = make(chan *Node)
@@ -76,6 +88,11 @@ func (i *Node) init(parent *SortedList) {
 }
 
 func (i *Node) Start() {
+	// This Loop emulates Locking via a select
+	// On the channels that access this Node
+	// In Theory This should work
+	// The cases I'm wondering about are like when both a req for the prev pointer and a req to set th prev
+	// Happen at the same time.  I think it will work but I haven't tested it yet
 	go func() {
 		i.status = "started"
 		for {
@@ -83,70 +100,75 @@ func (i *Node) Start() {
 			select {
 				case i.getStatus <- i.status:
 				case <- i.stop:
+					i.valServerStop <- 0
 					i.status = "stopped"
 					return
+				// Set the next Pointer
 				case next := <-i.setNext:
 					i.status = "working"
 					i.next = next
+				// Set the prev Pointer
 				case prev :=  <-i.setPrev:
 					i.status = "working"
 					i.prev = prev
+				// Return the next Pointer
 				case i.getNext <- i.next:
+				// Return the prev Pointer 
 				case i.getPrev <- i.prev:
+			}
+		}
+	}()
+	go func() {
+		for {
+			select {
+				// Stop this go routine
+				case <- valServerStop:
+				// Return Val
+				case getVal <- val:
+				// Set Val
+				case val <- setVal:
 			}
 		}
 	}()
 }
 
-func (i *Node) SetVal(val int) {
-	go func() {
-		i.setVal <- val
-	}()
-}
 
-func (i *Node) GetVal() (ret chan int) {
-	ret = make(chan int)
-	go func() {
-		temp := <-getVal
-		ret <- temp
-	}()
-	return ret
-}
-
-func (i *Node) SetNext(next *Node) {
-	go func() {
-		i.setNext <- next
-	}()
-}
-
-func (i *Node) SetPrev(prev *Node) {
-	go func() {
-		i.setPrev <- prev
-	}()
-}
-
-func (i *Node) Stop() {
-	go func() {
-		i.stop <- 0
-	}()
-}
 
 type SortedList struct {
 	WorkerPool
 	begin *Node
 	end *Node
+
+	Insert chan Comparable
+	//Contains chan interface {}
+	stop chan int
 	len int
 }
 
-func (il *SortedList) Insert(val int) {
-	if il.begin == nil {
-		il.begin = NewNode(val, il)
-		il.PushWorker(il.begin)
-	} else if il.end == nil {
-		il.end = NewNode(val, il)
-		il.PushWorker(il.end)
-		il.begin.SetNext(il.end)
-		il.end.SetPrev(il.begin)
-	}
+func NewSortedList() *SortedList {
+	newList := new(SortedList)
+	newList.Insert = make(chan Comparable)
+	newList.stop = make(chan int)
+	newList.len = 0
+	newList.PushWorker(newList)
+	return newList
+}
+
+func (sl *SortedList) Start() {
+	go func() {
+		for {
+			select {
+				case exitVal <- sl.stop:
+					return
+				case newVal <- sl.Insert:
+			}
+		}
+	}()
+}
+
+func (sl *SortedList) Stop() {
+	go func() {
+		sl.stop <- 0
+	}()
 }
 
