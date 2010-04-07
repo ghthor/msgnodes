afcf110821ad453139d25b5c04c19f7d0ec08a5e
Will Walthall <ghthor@gmail.com>
2010-04-07 18:24:12 -0400

    moved shutdownSignal into the someJunk dir, just incase I want it later -which knowing me I will

--- main/main.go
+++ main/main.go
@@ -12,8 +12,8 @@ import (
 	//"rand"
 	//i "ghthor/init"
 	//"ghthor/node"
-	bufNode "ghthor/node/buffer"
-	"ghthor/node"
+	//bufNode "ghthor/node/buffer"
+	//"ghthor/node"
 )
 
 func Errored(e os.Error, mesg string) bool {
@@ -25,24 +25,6 @@ func Errored(e os.Error, mesg string) bool {
 	return false
 }
 
-type HighPrior struct {
-	node.BaseMsg
-}
-
-func (hp *HighPrior) Priority() (uint64) {
-	return 0
-}
-
-type LowPrior struct {
-	node.BaseMsg
-}
-
-func (lp *LowPrior) Priority() (uint64) {
-	return 2
-}
-
-type Msg node.Msg
-
 func main() {
 	runtime.GOMAXPROCS(4)
 	ts, _, _ := os.Time()
@@ -50,56 +32,14 @@ func main() {
 	rand.Seed(ts)
 	fmt.Printf("\n\n");
 
-	buf := new(bufNode.BufferNode).Init(40, nil)
-	defer buf.Stop()
-	//defer buf.Dispose()
-	buf.Listen()
-
-	newMsg := make(chan Msg, 2)
-	defer close(newMsg)
-
-	// Feed the Buffer
-	go func() {
-		for !closed(newMsg) {
-			switch rand.Intn(10) {
-				case 0:
-					newMsg <- new(HighPrior)
-				case 1,2,3,4,5,6,7,8,9:
-					newMsg <- new(LowPrior)
-			}
-		}
-	}()
 
 	joinCh := make(chan bool)
 	defer close(joinCh)
 
-	numMsgs := 200
-	go func() {
-		// Fill Buffer with 10 Msgs
-		for i := 0; i < numMsgs; i++ {
-			if closed(newMsg) || closed(buf.In) { return }
-			msg := <-newMsg
-			buf.In <- msg
-		}
-	}()
-
-	var MsgsProcessed uint64 = 0
-
-	go func() {
-		for !closed(buf.MsgReq) {
-			msgChan := <-buf.MsgReq
-			if msgChan == nil { return }
-			msg := <-msgChan
-			if msg == nil { return }
-			msg.SetProcId(MsgsProcessed)
-			MsgsProcessed++
-			fmt.Printf("MsgPrior: %v \tRecvId: %v \tProcId: %v \tMsgsBuffered: %v\n", msg.Priority(), msg.RecvId(), msg.ProcId(), buf.MsgsBuffered)
-			if MsgsProcessed >= uint64(numMsgs) { joinCh <- true; return }
-		}
-	}()
-
-
-	<-joinCh
+	var Uint uint64 = 100
+	off := -1
+	Uint += off
+	//<-joinCh
 	time.Sleep(2000)
 	fmt.Printf("\n\n")
 }
--- node/atomic/uint64/atomic_int64.go
+++ node/atomic/uint64/atomic_int64.go
@@ -2,10 +2,50 @@ package atomic_int64
 
 import (
 	"ghthor/node"
+	"ghthor/node/buffer"
 )
 
+type Msg node.Msg
+
+type QueryVal struct {
+	node.BaseMsg
+	query chan Type
+}
+
+type SetTo struct {
+	node.BaseMsg
+	newVal Type
+}
+
+type QueryAndSet struct {
+	QueryVal
+}
+
+// Only Needed For uint's!
+type OffsetDir byte
+const (
+	Pos OffsetDir = iota;
+	Neg
+)
+
+type OffsetBy struct {
+	node.BaseMsg
+	Offset Type
+	Dir OffsetDir
+}
+
+type Monitor struct {
+	node.BaseMsg
+	comm chan Msg
+}
+
+type RemoveMonitor struct {
+	Monitor *Monitor
+}
+
 type AtomicNode struct {
-	node.BaseNode
+	buffer.BufferNode
+	//node.BaseNode
 	val Type
 }
 
