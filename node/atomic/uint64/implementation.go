5ab62f5a323eb7198a73fce01aa1f90d07e589f0
Will Walthall <ghthor@gmail.com>
2010-04-19 22:48:58 -0400

    Working on implementing the handlers for each message in processMsg

--- node/atomic/uint64/implementation.go
+++ node/atomic/uint64/implementation.go
@@ -1,4 +1,13 @@
 package atomic_uint64
 
 func (an *AtomicNode) processMsg(msg Msg) {
+	switch msg.(type) {
+		case QueryVal:
+			qv := msg.(QueryVal)
+			qv.Query <- an.Val
+		case QueryAndSet:
+			qs := msg.(QueryAndSet)
+			qs.Query <- an.Val
+			an.Val = <-qs.Query
+	}
 }
